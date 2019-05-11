3ec161e3fcc323c4433c4575f466878c2ca433bf
yangchen <1053364992@qq.com>
2019-05-11 20:13:20 +0800

    本地测试全部通过

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,46 @@
 package com.example.employee.entity;
 
-public class Company {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
+@Entity
+public class Company {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Column(name = "companyName")
+    private String companyName;
+
+    @NotNull
+    @Column(name = "employeesNumber")
+    private int employeesNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public void setEmployeesNumber(int employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
 }
--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,83 @@
 package com.example.employee.entity;
 
-public class Employee {
+import javax.persistence.*;
 
+@Entity
+@Table(name = "Employee")
+public class Employee {
+//    实体Employee包含字段分别有：id, 姓名(name), 年龄(age), 性别(gender), companyId, 薪资(salary)
+    @Id //定义属性为数据库的主键，每个实体必须要有一个
+    @Column(name = "id",nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //定义主键生成策略
+    private int id;
+
+    @Column(name = "name",nullable = false)
+    private String name;
+
+    @Column(name = "age",nullable = false)
+    private int age;
+
+    @Column(name = "gender",nullable = false)
+    private String gender;
+
+    @Column(name = "companyId",nullable = false)
+    private int companyId;
+
+    @Column(name = "salary",nullable = false)
+    private int salary;
+
+    public Employee(){}
+
+    public Employee(int id,String name,int age,String gender,int companyId,int salary){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.companyId = companyId;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(int companyId) {
+        this.companyId = companyId;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
